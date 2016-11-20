2a1673d1a522b077812547e96a4a9d3cb1299a4a
Damon Hutley <djh204@uclive.ac.nz>
2016-11-21 11:40:30 +1300

    Import ADC-DAC Pi library

--- ADC_DAC_Pi.py
+++ ADC_DAC_Pi.py
@@ -1,24 +1,46 @@
+'''
+Adaptation of DAQT7_Objective.py for the Raspberry Pi.
+
+Author: Damon Hutley
+Date: 21st November 2016
+'''
+
+
+from ABE_ADCDACPi import ADCDACPi
 import time
 import numpy as np
 import sys
 
+
 class DetectPi:
 	
 	def __init__(self):
-		
+		'''
+		Initialise ADC-DAC Pi.
+		'''
 	
 	def getDetails(self):
-		
+		'''
+		Return details of ADC_DAC Pi.
+		'''
 	
 	def writePort(self, Port, Volt):
-		
+		'''
+		Write values to a DAC pin.
+		'''
 	
 	def readPort(self, Port):
-		
+		'''
+		Read values from an ADC pin.
+		'''
 	
 	def streamRead(self, scanRate, scansPerRead, Port):
-		
+		'''
+		Read analogue input values from an ADC pin, in stream mode.
+		'''
 	
 	def close(self):
-		
+		'''
+		Close ADC-DAC Pi.
+		'''
 
