ca6e650d670cd0a987da9775bd561530d8ae8cfe
Damon Hutley <djh204@uclive.ac.nz>
2016-11-21 10:25:08 +1300

    Create class functions

---
+++ ADC_DAC_Pi.py
@@ -0,0 +1,24 @@
+import time
+import numpy as np
+import sys
+
+class DetectPi:
+	
+	def __init__(self):
+		
+	
+	def getDetails(self):
+		
+	
+	def writePort(self, Port, Volt):
+		
+	
+	def readPort(self, Port):
+		
+	
+	def streamRead(self, scanRate, scansPerRead, Port):
+		
+	
+	def close(self):
+		
+
