0fd2cd32d7f35d76b4a6bca9c1c67ef98b8f1311
Damon Hutley <djh204@uclive.ac.nz>
2016-11-21 13:22:19 +1300

    Convert DAQ ports to ADC-DAC channels

--- ADC_DAC_Pi.py
+++ ADC_DAC_Pi.py
@@ -16,40 +16,74 @@ class DetectPi:
 	
 	def __init__(self):
 		'''
-		Initialise ADC-DAC Pi.
+		Initialise ADC-DAC Pi. The DAC gain factor is set to 1, which allows 
+		the DAC output voltage to be set between 0 and 2.048 V. The DAC gain 
+		factor may also be set to 2, which gives an output range of 0 to 3.3 V.
 		'''
+		# Create instance of ADCDACPi, with gain set to 1
+		adcdac = ADCDACPi(1)
 		
-		adcdac = ADCDACPi(1) # Create instance of ADCDACPi, with gain set to 1
+		# Set reference voltage to 3.3 V
+		adcdac.set_adc_refvoltage(3.3)
 		
-		adcdac.set_adc_refvoltage(3.3) # Set reference voltage to 3.3 V
+		return
 		
 	
 	def getDetails(self):
 		'''
 		Return details of ADC_DAC Pi.
 		'''
+		
+		return
+		
 	
 	def writePort(self, Port, Volt):
 		'''
 		Write values to a DAC pin.
 		'''
 		
-		adcdac.set_dac_voltage(Port, Volt) # Set DAC output voltage <Volt> on channel <Port>
+		# Convert DAQT7 DAC ports to DAC Pi channels
+		if Port = "DAC0":
+			channel = 1
+		elif Port = "DAC1":
+			channel = 2
+		
+		# Set DAC output voltage <Volt> on channel <channel>
+		adcdac.set_dac_voltage(channel, Volt)
+		
+		return
+		
 	
 	def readPort(self, Port):
 		'''
 		Read values from an ADC pin.
 		'''
 		
-		adcdac.read_adc_voltage(Port, 0) # Read voltage from channel <Port> in single ended mode
+		# Convert DAQT7 AIN ports to ADC Pi channels
+		if Port = "AIN0":
+			channel = 1
+		elif Port = "AIN1":
+			channel = 2
+		
+		# Read voltage from channel <channel> in single ended mode
+		voltRead = adcdac.read_adc_voltage(channel, 0)
+		
+		return np.float(voltRead), time.time()
+		
 	
 	def streamRead(self, scanRate, scansPerRead, Port):
 		'''
 		Read analogue input values from an ADC pin, in stream mode.
 		'''
+		
 	
 	def close(self):
 		'''
 		Close ADC-DAC Pi.
 		'''
+		
+		pass
+
+
+		
 
