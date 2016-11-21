4fa2fc998216d4f58e57935e4498f256865d5660
Damon Hutley <djh204@uclive.ac.nz>
2016-11-21 16:40:47 +1300

    Add camera library functions

--- camera.py
+++ camera.py
@@ -1,10 +1,31 @@
 '''
-Control of the Raspberry Pi camera module using the picamera library.
+Library for the control of the Raspberry Pi camera module using picamera.
 
 Author: Damon Hutley
 Date: 21st November 2016
 '''
 
+
 from picamera import PiCamera
 
 
+class cameraModule:
+	
+	def __init__(self):
+		'''
+		Initialise the camera module class with picamera.
+		'''
+		
+		camera = PiCamera()
+		
+	
+	def capturePhoto(self):
+		
+	
+	def captureStream(self):
+		
+	
+	def networkStream(self):
+		
+	
+	def remoteControl(self):
